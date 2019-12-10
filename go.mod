3c2b205c4904b5d1cf6b229b7113b4820f37616e
Will Refvem <wbrefvem@gmail.com>
2019-12-10 10:32:19 -0500

    Add initial GitEvent controller implementation
    The GitEvent controller maps an event such as a PR or Issue comment to
    an action and creates an Action resource to manage this action.
    
    In this commit, the controller attempts to match a GitEvent to an action by
    listing all Actions, attempting to match the GitEvent to each Action's
    ParentEvent field, and then creating a new Action if no such match
    exists.

--- go.mod
+++ go.mod
@@ -10,7 +10,9 @@ require (
 	github.com/onsi/gomega v1.5.0
 	github.com/sirupsen/logrus v1.4.2
 	google.golang.org/appengine v1.5.0 // indirect
-	k8s.io/api v0.0.0-20190620084959-7cf5895f2711 // indirect
+	gopkg.in/src-d/go-git.v4 v4.13.1
+	gopkg.in/yaml.v2 v2.2.2
+	k8s.io/api v0.0.0-20190620084959-7cf5895f2711
 	k8s.io/apimachinery v0.0.0-20190816221834-a9f1d8a9c101
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
 	k8s.io/code-generator v0.0.0-20190912042602-ebc0eb3a5c23 // indirect
--- pkg/controllers/gitevent_controller.go
+++ pkg/controllers/gitevent_controller.go
@@ -17,6 +17,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	"github.com/sirupsen/logrus"
@@ -44,15 +45,39 @@ func (r *GitEventReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	// your logic here
 	logrus.Infof("reconciling the following resource: %s/%s", req.Namespace, req.Name)
 
+	actions := &foghornv1alpha1.ActionList{}
+	err := r.List(ctx, actions)
+	if err != nil {
+		logrus.WithError(err).Warn("failed to list Action resources")
+	}
+
+	// List Action objs to see if one already has this GitEvent, return if so
+	for _, action := range actions.Items {
+		name := fmt.Sprintf("%s/%s", action.Spec.ParentEvent.Namespace, action.Spec.ParentEvent.Name)
+		reqName := fmt.Sprintf("%s/%s", req.Namespace, req.Name)
+		if name == reqName {
+			return ctrl.Result{}, nil
+		}
+	}
+
+	// Get the GitEvent and assign it to our new Action
+	gitEvent := &foghornv1alpha1.GitEvent{}
+	err = r.Get(ctx, req.NamespacedName, gitEvent)
+	if err != nil {
+		logrus.WithError(err).Warn("failed to fetch GitEvent Resource")
+	}
+
 	a := &foghornv1alpha1.Action{
-		Spec: foghornv1alpha1.ActionSpec{},
+		Spec: foghornv1alpha1.ActionSpec{
+			ParentEvent: gitEvent,
+		},
 		ObjectMeta: metav1.ObjectMeta{
 			GenerateName: "action-",
 			Namespace:    req.Namespace,
 		},
 	}
 
-	err := r.Create(ctx, a)
+	err = r.Create(ctx, a)
 	if err != nil {
 		logrus.WithError(err).Warn("failed to create Action resource")
 	}
